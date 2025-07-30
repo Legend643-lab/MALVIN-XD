afbe97769a6b5764e0071822924c2e30c68f7813
Legend643-lab <hglegend643@gmail.com>
2025-07-30 20:49:55 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~Nq9n2ZRA#SpMmw_U4U5NFFAFhGV_XGU4NSlAVXVrIfmom_az4yrw",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2347072050737", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
